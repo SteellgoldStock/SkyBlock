9fa44e73107f6092e161f4a872497ab65add0974
Gaëtan H <steellgold0@gmail.com>
2022-12-04 16:52:34 +0100

    Envoi du formulaire + vérification

--- src/steellgold/skyblock/commands/IslandCommand.php
+++ src/steellgold/skyblock/commands/IslandCommand.php
@@ -3,8 +3,11 @@
 namespace steellgold\skyblock\commands;
 
 use CortexPE\Commando\BaseCommand;
+use Exception;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
 use steellgold\skyblock\commands\subs\IslandCreateCommand;
+use steellgold\skyblock\player\SkyBlockPlayer;
 
 class IslandCommand extends BaseCommand {
 
@@ -12,7 +15,14 @@ class IslandCommand extends BaseCommand {
 		$this->registerSubCommand(new IslandCreateCommand("create", "Create a new island"));
 	}
 
+	/**
+	 * @throws Exception
+	 */
 	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
-		$this->sendUsage();
+		if (!$sender instanceof Player) return;
+		$session = SkyBlockPlayer::get($sender);
+		if (!$session->hasIsland()) {
+
+		}
 	}
 }
