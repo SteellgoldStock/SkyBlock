099b4cfcdd14b57e88c0f3d2bc0238e4c0466ada
Gaëtan H <steellgold0@gmail.com>
2022-12-04 19:18:54 +0100

    Introduction de MySQL

--- src/steellgold/skyblock/SkyBlock.php
+++ src/steellgold/skyblock/SkyBlock.php
@@ -4,25 +4,43 @@ namespace steellgold\skyblock;
 
 use CortexPE\Commando\exception\HookAlreadyRegistered;
 use CortexPE\Commando\PacketHooker;
+use mysqli;
 use pocketmine\plugin\PluginBase;
 use steellgold\skyblock\commands\IslandCommand;
 use steellgold\skyblock\listeners\PlayerListeners;
+use steellgold\skyblock\utils\database\MySQL;
 
 class SkyBlock extends PluginBase {
+
+	public mysqli $mysql;
+
+	public static SkyBlock $instance;
 
 	/**
 	 * @throws HookAlreadyRegistered
 	 */
 	protected function onEnable(): void {
-		if(!PacketHooker::isRegistered()) {
+		self::$instance = $this;
+
+		if (!PacketHooker::isRegistered()) {
 			PacketHooker::register($this);
 		}
 
-		$this->getServer()->getCommandMap()->register("skyblock", new IslandCommand($this, "island","Commande principale du SkyBlock", ["is"]));
+		$this->saveResource("config.yml");
+		$this->mysql = MySQL::mysqli();
+		MySQL::createTable($this->mysql);
+
+		$this->getServer()->getCommandMap()->register("skyblock", new IslandCommand($this, "island", "Commande principale du SkyBlock", ["is"]));
 		$this->getServer()->getPluginManager()->registerEvents(new PlayerListeners(), $this);
 	}
 
-	protected function onDisable(): void {
+	/** @return SkyBlock */
+	public static function getInstance(): SkyBlock {
+		return self::$instance;
+	}
 
+	/** @return mysqli */
+	public function getMySQL(): mysqli {
+		return $this->mysql;
 	}
 }
---
+++ src/steellgold/skyblock/utils/database/MySQL.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace steellgold\skyblock\utils\database;
+
+use steellgold\skyblock\SkyBlock;
+
+class MySQL {
+
+	public static function mysqli(): \mysqli {
+		$config = SkyBlock::getInstance()->getConfig();
+		return new \mysqli(
+			$config->get("database")["host"],
+			$config->get("database")["username"],
+			$config->get("database")["password"],
+			$config->get("database")["database"],
+			$config->get("database")["port"]
+		);
+	}
+
+	public static function createTable(\mysqli $mysqli): void {
+		$mysqli->query("CREATE TABLE IF NOT EXISTS players (
+			id INT PRIMARY KEY NOT NULL,
+			player VARCHAR(100),
+			island VARCHAR(100) DEFAULT null
+		)");
+	}
+}
