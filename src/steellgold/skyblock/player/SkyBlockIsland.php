2abf617365793f3376bdc174e25e8d75e30b1c2d
Gaëtan H <steellgold0@gmail.com>
2022-12-04 19:19:11 +0100

    Fonctions pour l'île (membres, propriétaire ect)

--- src/steellgold/skyblock/player/SkyBlockIsland.php
+++ src/steellgold/skyblock/player/SkyBlockIsland.php
@@ -22,18 +22,64 @@ final class SkyBlockIsland {
 
 	/** @throws Exception */
 	private static function loadSessionData(Player $player) : SkyBlockIsland {
-		return new SkyBlockIsland($player->getXuid());
+		// TODO: Load data from database
+		return new SkyBlockIsland($player->getXuid(), $player->getName(), [$player->getName()]);
 	}
 
 	/**
 	 * @param int $identifier
+	 * @param string $owner
+	 * @param string[] $members
 	 */
 	public function __construct(
-		private int $identifier
+		private int $identifier,
+		private string $owner,
+		private array $members
 	){ }
 
 	/** @return int */
 	public function getIdentifier() : int{
 		return $this->identifier;
+	}
+
+	/** @param int $identifier */
+	private function setIdentifier(int $identifier): void {
+		$this->identifier = $identifier;
+	}
+
+	/** @return string */
+	public function getOwner(): string {
+		return $this->owner;
+	}
+
+	/** @param string $owner */
+	public function setOwner(string $owner): void {
+		$this->owner = $owner;
+	}
+
+	/** @return array */
+	public function getMembers(): array {
+		return $this->members;
+	}
+
+	/** @param array $members */
+	public function setMembers(array $members): void {
+		$this->members = $members;
+	}
+
+	/**
+	 * @param string $member
+	 * @return void
+	 */
+	public function addMember(string $member): void {
+		$this->members[] = $member;
+	}
+
+	/**
+	 * @param string $member
+	 * @return void
+	 */
+	public function removeMember(string $member): void {
+		$this->members = array_diff($this->members, [$member]);
 	}
 }
