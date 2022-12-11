3754bfeae37cd24c9e1e701038b4ae925df5b750
Gaëtan H <steellgold0@gmail.com>
2022-12-11 16:53:51 +0100

    Création du monde lors de `/is create`

--- src/steellgold/skyblock/commands/subs/IslandCreateCommand.php
+++ src/steellgold/skyblock/commands/subs/IslandCreateCommand.php
@@ -10,10 +10,12 @@ use dktapps\pmforms\element\Label;
 use dktapps\pmforms\ModalForm;
 use Exception;
 use pocketmine\command\CommandSender;
+use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use steellgold\skyblock\player\SkyBlockIsland;
 use steellgold\skyblock\player\SkyBlockPlayer;
 use steellgold\skyblock\utils\TextUtils;
+use steellgold\skyblock\utils\WorldUtils;
 
 class IslandCreateCommand extends BaseSubCommand {
 
@@ -36,19 +38,26 @@ class IslandCreateCommand extends BaseSubCommand {
 		$sender->sendForm(self::createIslandForm($session));
 	}
 
-	public static function createIslandForm(SkyBlockPlayer $player) : ModalForm {
+	public static function createIslandForm(SkyBlockPlayer $player): ModalForm {
 		return new ModalForm(
 			TextUtils::FORM_TITLE_MODAL,
 			"§d- §rVous êtes sur le point de créer une île. Êtes-vous sûr de vouloir continuer?",
 
-			function(Player $submitter, bool $choice) use ($player) : void{
+			function (Player $submitter, bool $choice) use ($player): void {
 				if ($choice) {
 					$island = new SkyBlockIsland($submitter->getXuid(), $submitter->getName(), $submitter->getName(), [$submitter->getName()]);
 					$player->setIsland($island);
-					$submitter->sendMessage(TextUtils::text("Vous venez de créer votre île « §d" .  $island->getIslandName() ." §f» avec succès!"));
+					$submitter->sendMessage(TextUtils::text("Vous venez de créer votre île « §d" . $island->getIslandName() . " §f» avec succès!"));
 					$submitter->sendForm(self::chooseIslandNameForm($submitter, $island));
-					// TODO: Create island world
-					// TODO: Teleport player to island
+
+					WorldUtils::duplicateWorld("copypaste", $submitter->getXuid());
+					$world = WorldUtils::getLoadedWorldByName($submitter->getXuid());
+					$world->setSpawnLocation(new Vector3(256, 71, 256));
+					if (WorldUtils::lazyLoadWorld("copypaste")) {
+						$submitter->teleport($world->getSpawnLocation());
+					} else {
+						$submitter->sendMessage(TextUtils::error("Une erreur est survenue lors du téléportation sur votre île."));
+					}
 				} else {
 					$submitter->sendMessage(TextUtils::error("Vous avez annulé la création de votre île."));
 				}
@@ -58,18 +67,18 @@ class IslandCreateCommand extends BaseSubCommand {
 		);
 	}
 
-	public static function chooseIslandNameForm(Player $player, SkyBlockIsland $island) : CustomForm {
+	public static function chooseIslandNameForm(Player $player, SkyBlockIsland $island): CustomForm {
 		return new CustomForm(
 			TextUtils::FORM_TITLE, [
-				new Label("description", "Choisissez un nom pour votre île, si vous ne le faites pas alors votre pseudo sera utilisé.\n\nVous pourrez très bien le changer à tout moment via la commande §d/is rename <nom>§r."),
-				new Input("name", "Nom de l'île", "Super île de " . $player->getName())
-			],
-			function(Player $submitter, CustomFormResponse $data) : void{
+			new Label("description", "Choisissez un nom pour votre île, si vous ne le faites pas alors votre pseudo sera utilisé.\n\nVous pourrez très bien le changer à tout moment via la commande §d/is rename <nom>§r."),
+			new Input("name", "Nom de l'île", "Super île de " . $player->getName())
+		],
+			function (Player $submitter, CustomFormResponse $data): void {
 				$island = SkyBlockPlayer::get($submitter)->getIsland();
 				$island->setIslandName($data->getString("name"));
 				$submitter->sendMessage(TextUtils::text("Vous venez de renommer votre île en §d" . $island->getIslandName() . "§r."));
 			},
-			function(Player $submitter) : void{
+			function (Player $submitter): void {
 				$submitter->sendMessage(TextUtils::error("Vous avez annulé le choix du nom de votre île. Elle sera donc nommée §fIle de " . $submitter->getName() . "§r."));
 			}
 		);
