d9b147f9d33e8c0cb5f958193829e497a1a471a5
Gaëtan H <steellgold0@gmail.com>
2022-12-12 19:44:44 +0100

    Bases des commandes: Invite & Teleport

--- src/steellgold/skyblock/commands/subs/IslandInviteCommand.php
+++ src/steellgold/skyblock/commands/subs/IslandInviteCommand.php
@@ -2,6 +2,19 @@
 
 namespace steellgold\skyblock\commands\subs;
 
-class IslandInviteCommand {
+use CortexPE\Commando\args\TargetArgument;
+use CortexPE\Commando\BaseSubCommand;
+use CortexPE\Commando\exception\ArgumentOrderException;
+use pocketmine\command\CommandSender;
 
+class IslandInviteCommand extends BaseSubCommand {
+
+	/** @throws ArgumentOrderException */
+	protected function prepare(): void {
+		$this->registerArgument(0, new TargetArgument("player",true));
+	}
+
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+		// TODO: Implement onRun() method.
+	}
 }
--- src/steellgold/skyblock/commands/subs/IslandTeleportCommand.php
+++ src/steellgold/skyblock/commands/subs/IslandTeleportCommand.php
@@ -2,6 +2,16 @@
 
 namespace steellgold\skyblock\commands\subs;
 
-class IslandTeleportCommand {
+use CortexPE\Commando\BaseSubCommand;
+use pocketmine\command\CommandSender;
 
+class IslandTeleportCommand extends BaseSubCommand {
+
+	protected function prepare(): void {
+		// TODO: Implement prepare() method.
+	}
+
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+		// TODO: Implement onRun() method.
+	}
 }
