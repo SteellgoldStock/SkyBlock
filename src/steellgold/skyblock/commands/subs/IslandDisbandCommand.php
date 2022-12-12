adb4280756d61e2c15b01568c063e6a25e86bec4
Gaëtan H <steellgold0@gmail.com>
2022-12-12 17:29:44 +0100

    Suppression de l'île

--- src/steellgold/skyblock/commands/subs/IslandDisbandCommand.php
+++ src/steellgold/skyblock/commands/subs/IslandDisbandCommand.php
@@ -7,7 +7,9 @@ use dktapps\pmforms\ModalForm;
 use Exception;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
+use steellgold\skyblock\player\SkyBlockIsland;
 use steellgold\skyblock\player\SkyBlockPlayer;
+use steellgold\skyblock\utils\database\MySQL;
 use steellgold\skyblock\utils\TextUtils;
 use steellgold\skyblock\utils\WorldUtils;
 
@@ -29,21 +31,21 @@ class IslandDisbandCommand extends BaseSubCommand {
 			return;
 		}
 
-		$sender->sendForm(self::disbandIslandForm($session));
+		$sender->sendForm(self::disbandIslandForm($session, $session->getIsland()));
 	}
 
-	public static function disbandIslandForm(SkyBlockPlayer $player): ModalForm {
+	public static function disbandIslandForm(SkyBlockPlayer $player, SkyBlockIsland $island): ModalForm {
 		return new ModalForm(
 			TextUtils::FORM_TITLE_MODAL,
 			"§d- §rVous êtes sur le point de supprimer votre île. Êtes-vous sûr de vouloir continuer? ceci est irréversible.",
 
-			function (Player $submitter, bool $choice) use ($player): void {
+			function (Player $submitter, bool $choice) use ($player, $island): void {
 				if ($choice) {
-					$world = WorldUtils::getLoadedWorldByName($submitter->getXuid());
-					WorldUtils::lazyUnloadWorld($world);
-					WorldUtils::removeWorld($submitter->getXuid());
-
+					WorldUtils::lazyUnloadWorld($island->getIdentifier());
+					WorldUtils::removeWorld($island->getIdentifier());
 					$player->setIsland(null);
+
+					MySQL::removeIsland($island->getIdentifier());
 					$submitter->sendMessage(TextUtils::text("Vous venez de supprimer votre île avec succès!"));
 				} else {
 					$submitter->sendMessage(TextUtils::error("Vous avez annulé la suppression de votre île."));
