67fa5411d62d67b267a1f1ff31ffeb29d7fb3145
Gaëtan H <steellgold0@gmail.com>
2022-12-12 21:03:48 +0100

    Certains changement à finir

--- src/steellgold/skyblock/commands/subs/IslandInviteCommand.php
+++ src/steellgold/skyblock/commands/subs/IslandInviteCommand.php
@@ -5,16 +5,50 @@ namespace steellgold\skyblock\commands\subs;
 use CortexPE\Commando\args\TargetArgument;
 use CortexPE\Commando\BaseSubCommand;
 use CortexPE\Commando\exception\ArgumentOrderException;
+use dktapps\pmforms\CustomForm;
+use dktapps\pmforms\CustomFormResponse;
+use dktapps\pmforms\element\Dropdown;
+use dktapps\pmforms\element\Input;
+use dktapps\pmforms\element\Label;
+use Exception;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\Server;
+use steellgold\skyblock\player\SkyBlockPlayer;
+use steellgold\skyblock\utils\TextUtils;
 
 class IslandInviteCommand extends BaseSubCommand {
 
-	/** @throws ArgumentOrderException */
+	/**
+	 * @throws ArgumentOrderException
+	 */
 	protected function prepare(): void {
-		$this->registerArgument(0, new TargetArgument("player",true));
+		$this->registerArgument(0, new TargetArgument("player", true));
 	}
 
+	/**
+	 * @throws Exception
+	 */
 	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
-		// TODO: Implement onRun() method.
+		if (!$sender instanceof Player) return;
+		$session = SkyBlockPlayer::get($sender);
+
+		if (!$session->hasIsland()) {
+			$sender->sendMessage(TextUtils::error("Vous n'avez pas d'île, créez une avant de pouvoir inviter des personnes."));
+			return;
+		}
+
+		if (!isset($args["player"])) $this->invitePlayerForm();
+		else $this->invitePlayerForm($session, $args["player"]);
+	}
+
+	public function invitePlayerForm(?string $option) : CustomForm {
+		$players = [];
+		foreach (Server::getInstance()->getOnlinePlayers() as $onlinePlayer) {
+			$players[] = $onlinePlayer->getName();
+		}
+
+		// If option was not set, define a dropdown with all online players, else define a label with the player name
+		$element = $option === null ? new Dropdown("player", "Choisissez un joueur à inviter", $players) : new Input("player", "Pseudonyme du joueur à inviter" . $option);
 	}
 }
