44267b8665d03d2977f4702c8bc2a43b7b273afb
Gaëtan H <steellgold0@gmail.com>
2022-12-15 19:38:04 +0100

    Code inutile

--- src/steellgold/skyblock/listeners/PlayerListeners.php
+++ src/steellgold/skyblock/listeners/PlayerListeners.php
@@ -8,24 +8,15 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use steellgold\skyblock\player\SkyBlockPlayer;
-use steellgold\skyblock\utils\database\MySQL;
 
 class PlayerListeners implements Listener {
 
 	/** @throws Exception */
 	public function onPlayerJoin(PlayerJoinEvent $event) {
-		// TODO: Check if player has an island
-		//       if island exists always teleport player to island
-		//       if island doesn't exist teleport player to spawn and clear inventory
-		//		 and notify player of island was deleted by owner
 	}
 
 	/** @throws Exception */
 	public function onPlayerQuit(PlayerQuitEvent $event) {
-		$player = $event->getPlayer();
-		$session = SkyBlockPlayer::get($player);
-
-		MySQL::mysqli()->query("UPDATE players SET island = '{$session->getIslandIdentifier()}' WHERE player = '{$player->getName()}'");
 	}
 
 	public function onPlayerMove(PlayerMoveEvent $event) {
