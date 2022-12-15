8a754d2cfc8d4f59c9081b2b6d6ca9b10eaaf7fa
Gaëtan H <steellgold0@gmail.com>
2022-12-15 19:39:38 +0100

    Introduction de IslandListCommand

---
+++ src/steellgold/skyblock/commands/subs/IslandListCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace steellgold\skyblock\commands\subs;
+
+use CortexPE\Commando\BaseSubCommand;
+use pocketmine\command\CommandSender;
+
+class IslandListCommand extends BaseSubCommand {
+
+	protected function prepare(): void {
+		// TODO: Implement prepare() method.
+	}
+
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+		// TODO: Implement onRun() method.
+	}
+}
