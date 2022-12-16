4bd7637c5d4334fcb7a3e922a52e3cf13e3c4d27
Gaëtan H <steellgold0@gmail.com>
2022-12-16 14:22:20 +0100

    Chemin d'accès

--- src/steellgold/skyblock/commands/subs/IslandKickCommand.php
+++ src/steellgold/skyblock/commands/subs/IslandKickCommand.php
@@ -1,9 +1,24 @@
 <?php
 
 namespace steellgold\skyblock\commands\subs;
+
 use CortexPE\Commando\args\TargetArgument;
 use CortexPE\Commando\BaseSubCommand;
+use CortexPE\Commando\exception\ArgumentOrderException;
+use dktapps\pmforms\CustomForm;
+use dktapps\pmforms\CustomFormResponse;
+use dktapps\pmforms\element\Dropdown;
+use dktapps\pmforms\element\Input;
+use dktapps\pmforms\element\Label;
+use dktapps\pmforms\element\Toggle;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\Server;
 use steellgold\skyblock\player\SkyBlockIsland;
+use steellgold\skyblock\player\SkyBlockPlayer;
+use steellgold\skyblock\utils\Invites;
+use steellgold\skyblock\utils\TextUtils;
+
 class IslandKickCommand extends BaseSubCommand {
 	protected function prepare(): void {
 		$this->registerArgument(0, new TargetArgument("player", true));
