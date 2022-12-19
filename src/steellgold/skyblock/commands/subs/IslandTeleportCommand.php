4b81bd8e6081c36f8b11900dbbeb6745d6a210da
Gaëtan H <steellgold0@gmail.com>
2022-12-19 15:56:07 +0100

    Commande de téléportation

--- src/steellgold/skyblock/commands/subs/IslandTeleportCommand.php
+++ src/steellgold/skyblock/commands/subs/IslandTeleportCommand.php
@@ -4,6 +4,10 @@ namespace steellgold\skyblock\commands\subs;
 
 use CortexPE\Commando\BaseSubCommand;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use steellgold\skyblock\player\SkyBlockPlayer;
+use steellgold\skyblock\utils\TextUtils;
+use steellgold\skyblock\utils\WorldUtils;
 
 class IslandTeleportCommand extends BaseSubCommand {
 
@@ -12,6 +16,14 @@ class IslandTeleportCommand extends BaseSubCommand {
 	}
 
 	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
-		// TODO: Implement onRun() method.
+		if (!$sender instanceof Player) return;
+		$session = SkyBlockPlayer::get($sender);
+
+		if (!$session->hasIsland()) {
+			$sender->sendMessage(TextUtils::error("Vous n'avez pas d'île, créer-en une d'abord pour pouvoir s'y téléporter"));
+			return;
+		}
+
+		$island = $session->getIsland();
 	}
 }
