3b4882cb4ee88f49691eecfbfc2765140cfa2251
Gaëtan H <steellgold0@gmail.com>
2022-12-20 12:31:47 +0100

    Membre

---
+++ src/steellgold/skyblock/player/roles/Member.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace steellgold\skyblock\player;
+
+class Member extends Role {
+
+	public function getName(): string {
+		return "Membre";
+	}
+
+	public function getPermissions(): array {
+		return [
+			self::PERMISSION_KICK_VISITOR,
+
+			self::PERMISSION_OPEN_ALL_CHESTS,
+			self::PERMISSION_BREAK_BLOCKS,
+			self::PERMISSION_PLACE_BLOCKS
+		];
+	}
+}
