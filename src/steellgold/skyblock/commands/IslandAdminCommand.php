3bacaf0884ad549bf3ae5112164958fbd2a2314b
Gaëtan H <steellgold0@gmail.com>
2022-12-20 01:17:57 +0100

    Enregistrement des deux commandes admin

--- src/steellgold/skyblock/commands/IslandAdminCommand.php
+++ src/steellgold/skyblock/commands/IslandAdminCommand.php
@@ -6,13 +6,15 @@ use CortexPE\Commando\BaseCommand;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\Server;
-use steellgold\skyblock\commands\subs\IslandStartChestCommand;
+use steellgold\skyblock\commands\subs\admin\IslandAdminStartChestCommand;
+use steellgold\skyblock\commands\subs\admin\IslandAdminDefaultTeleportCommand;
 use steellgold\skyblock\utils\TextUtils;
 
 class IslandAdminCommand extends BaseCommand {
 
 	protected function prepare(): void {
-		$this->registerSubCommand(new IslandStartChestCommand("chest","Edit the coords, or content of start chest"));
+		$this->registerSubCommand(new IslandAdminStartChestCommand("chest","Edit the coords, or content of start chest"));
+		$this->registerSubCommand(new IslandAdminDefaultTeleportCommand("defaultteleport","Teleport to the default world",["deftp"]));
 	}
 
 	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
