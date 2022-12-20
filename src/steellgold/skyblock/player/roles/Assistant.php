8351a868e3483b6df0856cabedda6fa4a9cec289
Gaëtan H <steellgold0@gmail.com>
2022-12-20 12:31:51 +0100

    Assistant

---
+++ src/steellgold/skyblock/player/roles/Assistant.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace steellgold\skyblock\player;
+
+class Assistant extends Role {
+
+	public function getName(): string {
+		return "Assistant";
+	}
+
+	public function getPermissions(): array {
+		return [
+			self::PERMISSION_KICK_MEMBER,
+			self::PERMISSION_KICK_VISITOR,
+
+			self::PERMISSION_OPEN_ALL_CHESTS,
+			self::PERMISSION_BREAK_BLOCKS,
+			self::PERMISSION_PLACE_BLOCKS
+		];
+	}
+}
